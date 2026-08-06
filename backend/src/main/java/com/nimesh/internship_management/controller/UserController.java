f46a5af94932d395c94e6ca79cb483ecba6ffa57
NimeshAlwis <nimeshalwis13@gmail.com>
2026-08-06 10:35:25 +0530

    Complete User CRUD APIs

--- backend/src/main/java/com/nimesh/internship_management/controller/UserController.java
+++ backend/src/main/java/com/nimesh/internship_management/controller/UserController.java
@@ -2,9 +2,7 @@ package com.nimesh.internship_management.controller;
 
 import com.nimesh.internship_management.model.User;
 import com.nimesh.internship_management.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +19,27 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id,
+            @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
     }
 
 }
